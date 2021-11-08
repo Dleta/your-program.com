9ebcf90ca4d7056c63778fdf87d2dd68b313eafc
Delta Bravo <76425899+Dleta@users.noreply.github.com>
2021-11-08 10:43:11 +0000

    add lock and shutdown script

---
+++ downloads/lock.pyw
@@ -0,0 +1,6 @@
+import time 
+import os
+
+while True:
+  time.sleep(60)
+  os.system("Rundll32.exe user32.dll,LockWorkStation")
---
+++ downloads/shutdown_timed.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+time = input("in how much seconds do you want do shutdown: ")
+time = int(time)
+
+time.sleep(time)
+
+os.system("shutdown /s /t 0")
