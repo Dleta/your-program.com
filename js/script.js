daff7ca1717a88b41d48b4df4858d345ef084676
Delta Bravo <76425899+Dleta@users.noreply.github.com>
2021-12-17 09:46:32 +0100

    add mobile nav

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-const btnX = document.querySelector(".mobile-nav-close-btn");
-const btnM = document.querySelector(".mobile-nav-open-btn");
+const btnMobNav = document.querySelector(".mob-btn");
 const headerEl = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
 
-btnM.addEventListener("click", function () {
-  headerEl.classList.add("nav-open");
+btnMobNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
-btnX.addEventListener("click", function () {
-  headerEl.classList.remove("nav-open");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.remove("nav-open");
+  });
 });
