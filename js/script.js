84b2594f8612f88652ef6873c73220cf769ea227
Delta Bravo <76425899+DanielButlerIsMyName@users.noreply.github.com>
2022-03-31 13:40:19 +0200

    update download nav

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
-const btnMobNav = document.querySelector(".mob-btn");
-const headerEl = document.querySelector(".header");
-const allLinks = document.querySelectorAll("a:link");
-try{
-  btnMobNav.addEventListener("click", function () {
-    headerEl.classList.toggle("nav-open");
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btnMobNav = document.querySelector(".mob-btn");
+  const headerEl = document.querySelector(".header");
+  const allLinks = document.querySelectorAll("a:link");
+  const downloadMobNavBtn = document.querySelector(".download-nav-btn")
+  const downloadNav = document.querySelector(".download-nav")
+
+  try{
+    btnMobNav.addEventListener("click", function () {
+      headerEl.classList.toggle("nav-open");
+    });
+  }catch{}
+
+
+  allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      const href = link.getAttribute("href");
+
+      // Close mobile navigation                      might be posibel with a an eventlistener on class mian-nav-link
+      if (link.classList.contains("main-nav-link"))
+        headerEl.classList.remove("nav-open");
+    });
   });
-}catch{}
 
+  downloadMobNavBtn.addEventListener("click", () => {
+    downloadNav.classList.toggle("hide");
+  })
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    const href = link.getAttribute("href");
-
-    // Close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.remove("nav-open");
-  });
-});
-
+})
